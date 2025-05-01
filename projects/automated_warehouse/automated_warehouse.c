1e2bb8f17fee89d2e68504f2fc1840bbc7fa50ea
1unaram <lunaram620@cau.ac.kr>
2025-05-01 20:29:19 +0900

    Feat: automated_warehouse.c

--- projects/automated_warehouse/automated_warehouse.c
+++ projects/automated_warehouse/automated_warehouse.c
@@ -11,15 +11,44 @@
 #include "projects/automated_warehouse/aw_manager.h"
 
 struct robot* robots;
+int robot_count;
+char **robot_tasks;
+char **robot_names;
 
-// test code for central control node thread
-void test_cnt(){
-        while(1){
-                print_map(robots, 4);
-                thread_sleep(1000);
-                block_thread();
+void parse_parameters(char **argv) {
+
+        // 1) Number of robots
+        robot_count = atoi(argv[1]);
+
+        // 2) Robot tasks
+        robot_tasks = malloc(sizeof(char *) * robot_count);
+
+        char buffer[256];
+        strlcpy(buffer, argv[2], sizeof(buffer));
+        buffer[sizeof(buffer) - 1] = '\0';
+
+        char *saveptr;
+        char *token = strtok_r(buffer, ":", &saveptr);
+        int index = 0;
+
+        while (token && index < robot_count) {
+                robot_tasks[index] = malloc(3);
+                strlcpy(robot_tasks[index], token, 3);
+                robot_tasks[index][sizeof(robot_tasks[index]) - 1] = '\0';
+                index++;
+                token = strtok_r(NULL, ":", &saveptr);
         }
 }
+
+// Print each robot's name and task
+void print_robot_info() {
+        printf("Robot Info:\n");
+        for (int i = 0; i < robot_count; i++) {
+                printf("%s -> %s\n", robot_names[i], robot_tasks[i]);
+        }
+        printf("\n");
+}
+
 
 // test code for robot thread
 void test_thread(void* aux){
@@ -31,28 +60,54 @@ void test_thread(void* aux){
         }
 }
 
+// test code for central control node thread
+void cnn_thread(){
+        while(1){
+                print_map(robots, robot_count);
+                thread_sleep(1000);
+                block_thread();
+        }
+}
+
 // entry point of simulator
 void run_automated_warehouse(char **argv)
 {
         init_automated_warehouse(argv); // do not remove this
 
-        printf("implement automated warehouse!\n");
 
-        // test case robots
-        robots = malloc(sizeof(struct robot) * 4);
-        setRobot(&robots[0], "R1", 5, 5, 0, 0);
-        setRobot(&robots[1], "R2", 0, 2, 0, 0);
-        setRobot(&robots[2], "R3", 1, 1, 1, 1);
-        setRobot(&robots[3], "R4", 5, 5, 0, 0);
+        printf("\n=========== Start ===========\n\n");
+        printf("implement automated warehouse!\n\n");
 
-        // example of create thread
-        tid_t* threads = malloc(sizeof(tid_t) * 4);
-        int idxs[4] = {1, 2, 3, 4};
+
+        // [1] Parse parameters
+        parse_parameters(argv);
+
+
+        // [2] Create and Set robots
+        robots = malloc(sizeof(struct robot) * robot_count);
+        robot_names = malloc(sizeof(char *) * robot_count);
+
+        for (int i = 0; i < robot_count; i++){
+                robot_names[i] = malloc(sizeof(char) * 3);
+                snprintf(robot_names[i], sizeof(robot_names), "R%d", i + 1);
+                setRobot(&robots[i], robot_names[i], 5, 5, 0, 0);
+        }
+
+        // [3] Print robot info
+        print_robot_info();
+
+        // [4] Create threads
+        // 1) Mallocate threads for ccn and robots
+        tid_t* threads = malloc(sizeof(tid_t) * (robot_count + 1));
+
+        // 2) Create thread for cnt
         threads[0] = thread_create("CNT", 0, &test_cnt, NULL);
-        threads[1] = thread_create("R1", 0, &test_thread, &idxs[1]);
-        threads[2] = thread_create("R2", 0, &test_thread, &idxs[2]);
-        threads[3] = thread_create("R3", 0, &test_thread, &idxs[3]);
 
-        // if you want, you can use main thread as a central control node
-        
+        // 3) Create threads for robots
+        int idxs[6] = {1, 2, 3, 4, 5, 6};
+        for (int i = 0; i < robot_count; i++){
+                threads[i + 1] = thread_create(robots[i].name, 0, &test_thread, &idxs[i + 1]);
+        }
+
+        printf("=========== End ===========\n\n");
 }
---
+++ run_warehouse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <num> <layout>"
+  echo "Example: $0 5 2A:4C:2B:2C:3A"
+  exit 1
+fi
+
+NUM=$1
+LAYOUT=$2
+
+echo "[+] Cleaning and building..."
+cd threads/build/
+make clean > ../../make_clean_result 2>&1
+cd ../
+make > ../make_result 2>&1
+
+echo "[+] Running Pintos with automated_warehouse $NUM $LAYOUT..."
+cd build/
+../../utils/pintos automated_warehouse "$NUM" "$LAYOUT"
+
